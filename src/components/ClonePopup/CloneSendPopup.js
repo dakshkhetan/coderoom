d5468ec711b40f89be2598d56590935b09c0e958
Daksh Khetan <2012daksh@gmail.com>
2020-11-02 23:28:10 +0530

    prettify files (#2)

--- src/components/ClonePopup/CloneSendPopup.js
+++ src/components/ClonePopup/CloneSendPopup.js
@@ -1,40 +1,35 @@
-import React, { Component } from 'react';
-import { Modal, Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './CloneSendPopup.css';
+import React, { Component } from "react";
+import { Modal, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./CloneSendPopup.css";
 
-export default class CloneSendPopup extends Component {    
+export default class CloneSendPopup extends Component {
+  render() {
+    return (
+      <Modal
+        {...this.props}
+        size="sm"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Clone sent!
+          </Modal.Title>
+        </Modal.Header>
 
-    render(){
+        <Modal.Body>
+          <div className="popup-heading">
+            Created {this.props.num} clone(s).
+          </div>
+        </Modal.Body>
 
-        return (
-            <Modal
-            {...this.props}
-            size="sm"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-            >
-
-                <Modal.Header closeButton>
-                    <Modal.Title id="contained-modal-title-vcenter">
-                        Clone sent!
-                    </Modal.Title>
-                </Modal.Header>
-
-                <Modal.Body>
-                    <div className="popup-heading" >
-                        Created {this.props.num} clone(s).
-                    </div>
-                </Modal.Body>
-                
-                <Modal.Footer>
-                    <Button variant="danger" onClick={this.props.onHide}>
-                        Close
-                    </Button>
-                </Modal.Footer>
-
-            </Modal>
-        );
-    }
-
+        <Modal.Footer>
+          <Button variant="danger" onClick={this.props.onHide}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
 }
